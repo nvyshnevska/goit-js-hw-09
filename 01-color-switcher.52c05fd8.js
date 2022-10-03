85fcb7311faefb5161bcefe249a20d490f0189f8
nvyshnevska <105102392+nvyshnevska@users.noreply.github.com>
2022-10-03 20:47:38 +0000

    Deploying to gh-pages from @ nvyshnevska/goit-js-hw-09@ee50dcdc893c9bf2e1af154e9e9b71901e953401 🚀

---
+++ 01-color-switcher.52c05fd8.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null,n=!1;t.startBtn.addEventListener("click",(function(){n||(n=!0,e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.body.style.backgroundColor=e}),1e3))})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),n=!1}));
+//# sourceMappingURL=01-color-switcher.52c05fd8.js.map
---
+++ 01-color-switcher.ce0d5016.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")},n=null,e=!1;t.startBtn.addEventListener("click",(function(){e||(e=!0,n=setInterval((function(){var n="#".concat(Math.floor(16777215*Math.random()).toString(16));t.body.style.backgroundColor=n}),1e3))})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),e=!1}))}();
+//# sourceMappingURL=01-color-switcher.ce0d5016.js.map
